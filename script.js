b5587f21b04652e060f9f5b62cc9c881bbb62662
YaAlan1 <chill.alanus@gmail.com>
2024-08-08 15:19:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let n = prompt("Введите число")
+let s = prompt("Введите знак")
+let y = prompt("Введите число")
+
+if (s == "-") {
+    console.log(n - y);
+}
+else if (s == "+") {
+    console.log(n + y);
+}
+else if (s == "*") {
+    console.log(n * y);
+}
+else if (s == "/") {
+    console.log(n / y);
+}
